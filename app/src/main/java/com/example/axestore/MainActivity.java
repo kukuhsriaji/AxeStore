93cda928798e43439011cb487d2a4611c89b3923
Sarkemboy <gondarface122@gmail.com>
2020-07-16 22:48:31 +0700

    tampilan diperbarui
    tampilan diperbarui,,,mau nanya mas kan saya pake viewflipper nah kalo mau ditambah text gimana ya biar auto ngikut juga text nya?

--- app/src/main/java/com/example/axestore/MainActivity.java
+++ app/src/main/java/com/example/axestore/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.axestore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+    ViewFlipper v_flipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int images[] = {R.drawable.koi1, R.drawable.mas_koki1, R.drawable.molly_marble1};
+
+
+        v_flipper = findViewById(R.id.v_flipper);
+
+        for (int image: images){
+            flipperImages(image);
+        }
+    }
+    public void flipperImages(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(3000);
+        v_flipper.setAutoStart(true);
+
+        //animation
+
+        v_flipper.setInAnimation(this, android.R.anim.slide_in_left);
+        v_flipper.setOutAnimation(this, android.R.anim.slide_out_right);
     }
 }
